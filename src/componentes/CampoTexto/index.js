e2df6af7884ecd12e31a4faad39f97019263c07c
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2023-04-08 20:57:31 -0300

    update botao + display data user

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -3,10 +3,20 @@ import './CampoTexto.css'
 // const placeholderModificada = `${props.placeholder}` ex: usar placeholderModificada no lugar de props.placeholder
 
 const CampoTexto = (props) => {
+
+  const aoDigitado = (evento) => {
+    props.aoAlterado(evento.target.value)
+  }
+
   return (
     <div className="campo-texto">
       <label>{props.label}</label>
-      <input placeholder={props.placeholder}/>
+      <input 
+        onChange={aoDigitado} 
+        placeholder={props.placeholder}
+        required={props.obrigatorio} 
+        value={props.valor} 
+      />
     </div>
   );
 };
--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -4,7 +4,11 @@ const ListaSuspensa = (props) => {
     return (
         <div className='listaSuspensa'>
             <label>{props.label}</label>
-            <select>
+            <select 
+                onChange={evento => props.aoAlterado(evento.target.value)} 
+                required={props.obrigatorio} 
+                value={props.value}
+            >
                 {props.itens.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
